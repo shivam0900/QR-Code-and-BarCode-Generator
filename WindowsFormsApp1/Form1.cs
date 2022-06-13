1615936e6d8bb8b9fb383a22b7de1b20af4cfce6
Shivam Chaturvedi <shivamchaturvedi0400@gmail.com>
2022-06-13 16:22:41 +0530

    importing file

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApp1
+{
+    public partial class QrBarcodeGenerator : Form
+    {
+        bool isGenerated = false;
+        public QrBarcodeGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isGenerated = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            Zen.Barcode.CodeQrBarcodeDraw qrBarcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pictureBox1.Image = qrBarcode.Draw(textBox1.Text, 200);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isGenerated = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            Zen.Barcode.Code128BarcodeDraw bar = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = bar.Draw(textBox2.Text, 200);
+        }
+
+        private void QrBarcodeGenerator_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(isGenerated)
+            {
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            pictureBox1.Image.Save(path + "\\" + DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString() + ".jpg", ImageFormat.Jpeg);
+
+            }
+        }
+    }
+}
